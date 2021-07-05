0d7e9a48d506ea03e0745cb34b392af44d4c5a69
Felipe Figueiredo <philsf79@gmail.com>
2021-07-05 20:37:48 -0300

    honrar escolha do usuário: cores

--- data-raw/Entorno03.R
+++ data-raw/Entorno03.R
@@ -3,7 +3,7 @@ library(tidyverse)
 ## localização dos dados brutos
 censo_dir <- file.path(paste0(Sys.getenv("HOME"), "/Downloads/Censo2010/"))
 
-cores <- 1
+if(is.null(cores)) cores <- 1
 
 Entorno03 <- abrir_base(base = "Entorno03", cores = cores)
 
--- data-raw/Pessoa03.R
+++ data-raw/Pessoa03.R
@@ -3,7 +3,7 @@ library(tidyverse)
 ## localização dos dados brutos
 censo_dir <- file.path(paste0(Sys.getenv("HOME"), "/Downloads/Censo2010/"))
 
-cores <- 1
+if(is.null(cores)) cores <- 1
 
 Pessoa03 <- abrir_base(base = "Pessoa03", cores = cores)
 
