bd196fa792ff40b07ab08240f984840dc6386926
Felipe Figueiredo <philsf79@gmail.com>
2021-07-05 19:47:24 -0300

    bacon retirado

--- data-raw/Basico.R
+++ data-raw/Basico.R
@@ -1,15 +1,10 @@
-## code to prepare `censo2010` dataset goes here
-
 library(tidyverse)
-
-source("data-raw/abrir_base_estado.R")
 
 ## localização dos dados brutos
 censo_dir <- file.path(paste0(Sys.getenv("HOME"), "/Downloads/Censo2010/"))
 
-base <- "Basico"
 cores <- 1
 
-Basico <- abrir_base(base, cores = cores)
+Basico <- abrir_base(base = "Basico", cores = cores)
 
 usethis::use_data(Basico, overwrite = TRUE)
