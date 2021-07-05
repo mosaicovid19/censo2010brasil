abcdb92b6a9ded117aa97ed9e754423b3d06f3bd
Felipe Figueiredo <philsf79@gmail.com>
2021-07-05 20:28:52 -0300

    Entorno03

---
+++ data-raw/Entorno03.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+
+## localização dos dados brutos
+censo_dir <- file.path(paste0(Sys.getenv("HOME"), "/Downloads/Censo2010/"))
+
+cores <- 1
+
+Entorno03 <- abrir_base(base = "Entorno03", cores = cores)
+
+usethis::use_data(Entorno03, overwrite = TRUE)
