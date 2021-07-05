ced4792667a543ac25c8a6b221d9f9f4b0f0cad1
Felipe Figueiredo <philsf79@gmail.com>
2021-07-05 20:03:22 -0300

    abrir_base_estado generalizada (nome original da base)

--- data-raw/abrir_base_estado.R
+++ data-raw/abrir_base_estado.R
@@ -1,15 +1,7 @@
 abrir_base_estado <- function(base, estado, censodir = "~/Downloads/Censo2010/") {
-  arquivos <- c(
-    Basico = file.path(censo_dir, paste0("Basico_", estado, ".xls")),
-    Entorno = file.path(censo_dir, paste0("Entorno03_",estado,".xls")),
-    Domicilio01 = file.path(censo_dir, paste0("Domicilio01_",estado,".xls")),
-    Domicilio02 = file.path(censo_dir, paste0("Domicilio02_",estado,".xls")),
-    Pessoa = file.path(censo_dir, paste0("Pessoa03_",estado,".xls")),
-    Dom.Renda = file.path(censo_dir, paste0("DomicilioRenda_",estado,".xls")),
-    Resp.Alfa = file.path(censo_dir, paste0("Responsavel02_",estado,".xls"))
-  )
-  b <- readxl::read_excel(arquivos[base], na = "X")
-  print(paste(estado, base, arquivos[base]))
+  arquivo <- file.path(censo_dir, paste0(base, "_", estado, ".xls"))
+  b <- readxl::read_excel(arquivo, na = "X")
+  print(paste(estado, base, arquivo))
   b %>%
     # fix Entorno03
     mutate(Cod_UF = estado) %>%
