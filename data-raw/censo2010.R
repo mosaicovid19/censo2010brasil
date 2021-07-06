512792817c09bb59367a0adc45fcd5a4f7832df6
Felipe Figueiredo <philsf79@gmail.com>
2021-07-05 21:23:27 -0300

    script geral para rodar todas as rotinas individuais

--- data-raw/censo2010.R
+++ data-raw/censo2010.R
@@ -2,34 +2,20 @@
 
 library(tidyverse)
 
-source("data-raw/abrir_base_estado.R")
+source("data-raw/abrir_base_estado.R", encoding = 'UTF-8')
+source("data-raw/abrir_base.R", encoding = 'UTF-8')
 
 ## localização dos dados brutos
 censo_dir <- file.path("~/Downloads/Censo2010")
 
-bases <- c("Basico", "Entorno", "Domicilio01", "Domicilio02", "Pessoa",
-           "Dom.Renda", "Resp.Alfa")
+# num de cores para usar em paralelo
+# não é recomendado usar muitos cores, devido ao tamanho das bases
+if(!exists("cores")) cores <- 1
 
-estados <- c("AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA",
-             "MG", "MS", "MT", "PA", "PB", "PE", "PI", "RJ", "RS", "SP1",
-             "SP2", "TO")
-
-# estados <- c("AC", "AP") # UF pequenas para teste
-# bases <- c("Basico", "Entorno")
-# bases <- "Resp.Alfa"
-
-cores <- detectCores() %/% 2
-cores <- 2
-# cl <- makePSOCKcluster(detectCores())
-# registerDoParallel(cl)
-registerDoParallel(cores = cores)
-
-censo2010 <- foreach(b = bases, .packages = c("readxl"), .export = c("abrir_base_estado")) %:%
-  foreach(e = estados, .combine = bind_rows) %do%
-  abrir_base_estado(b, e, censo_dir)
-names(censo2010) <- bases
-
-# stopCluster(cl)
-stopImplicitCluster()
-
-usethis::use_data(censo2010, overwrite = TRUE)
+source("data-raw/Basico.R", encoding = 'UTF-8')
+source("data-raw/Domicilio01.R", encoding = 'UTF-8')
+source("data-raw/Domicilio02.R", encoding = 'UTF-8')
+source("data-raw/DomicilioRenda.R", encoding = 'UTF-8')
+source("data-raw/Entorno03.R", encoding = 'UTF-8')
+source("data-raw/Pessoa03.R", encoding = 'UTF-8')
+source("data-raw/Responsavel02.R", encoding = 'UTF-8')
