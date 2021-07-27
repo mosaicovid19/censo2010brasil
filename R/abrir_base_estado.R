3277b3a9c402750af94d3f3d366073abcafacad1
Felipe Figueiredo <philsf79@gmail.com>
2021-07-27 20:10:58 -0300

    fóssil removido

--- R/abrir_base_estado.R
+++ R/abrir_base_estado.R
@@ -1,7 +1,8 @@
 abrir_base_estado <- function(base, estado, censodir = "~/Downloads/Censo2010/") {
   arquivo <- file.path(censo_dir, paste0(base, "_", estado, ".xls"))
+
   b <- readxl::read_excel(arquivo, na = "X")
-  print(paste(estado, base, arquivo))
+
   b %>%
     # fix Entorno03
     mutate(Cod_UF = estado) %>%
