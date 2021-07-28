8f4526d06a625d298db26605c7acedd235e9e6a8
Felipe Figueiredo <philsf79@gmail.com>
2021-07-28 01:28:16 -0300

    templates roxygen das funções (sinew)

--- R/abrir_base.R
+++ R/abrir_base.R
@@ -1,3 +1,24 @@
+#' @title FUNCTION_TITLE
+#' @description FUNCTION_DESCRIPTION
+#' @param base PARAM_DESCRIPTION
+#' @param estados PARAM_DESCRIPTION, Default: NULL
+#' @param censo_dir PARAM_DESCRIPTION, Default: '~/Downloads/Censo2010'
+#' @param cores PARAM_DESCRIPTION, Default: 1
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @seealso
+#'  \code{\link[doParallel]{c("registerDoParallel", "registerDoParallel")}},\code{\link[doParallel]{registerDoParallel}}
+#'  \code{\link[foreach]{foreach}}
+#' @rdname abrir_base
+#' @export
+#' @importFrom doParallel registerDoParallel stopImplicitCluster
+#' @importFrom foreach foreach
 abrir_base <- function(base, estados = NULL, censo_dir = "~/Downloads/Censo2010", cores = 1) {
 
   require(doParallel) # construto %dopar%
