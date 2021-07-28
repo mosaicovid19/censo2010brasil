a7a273d0f059106b963f69e2f5bea23f1638f739
Felipe Figueiredo <philsf79@gmail.com>
2021-07-27 21:54:43 -0300

    fix: sintaxe corrigida (obs)
    - any_of espera string
    - a função a ser aplicada foi convertida para sintaxe purrr

--- R/abrir_base_estado.R
+++ R/abrir_base_estado.R
@@ -7,7 +7,7 @@ abrir_base_estado <- function(base, estado, censo_dir = "~/Downloads/Censo2010/"
     # fix Basico: substitui Cod_UF pelo código (numérico)
     # Assume que Cod_meso sempre existe na base em que Cod_UF existe e
     # toma os 2 primeiros dígitos de um número de 4 dígitos
-    mutate( across(any_of(Cod_UF), Cod_meso %/% 100) ) %>%
+    mutate( across(any_of("Cod_UF"), ~ Cod_meso %/% 100) ) %>%
     # fix Responsavel02_SP2
     mutate(across(starts_with("V"), as.numeric))
 }
