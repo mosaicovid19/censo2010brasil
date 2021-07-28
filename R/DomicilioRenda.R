46a37036eb39380d40e27882759baed617f79ec0
Felipe Figueiredo <philsf79@gmail.com>
2021-07-28 14:26:20 -0300

    templates de doc dos data.frames

---
+++ R/DomicilioRenda.R
@@ -0,0 +1,23 @@
+#' @title DATASET_TITLE
+#' @description DATASET_DESCRIPTION
+#' @format A data frame with 310120 rows and 16 variables:
+#' \describe{
+#'   \item{\code{Cod_setor}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{Situacao_setor}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{V001}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{V002}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{V003}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{V004}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{V005}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{V006}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{V007}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{V008}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{V009}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{V010}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{V011}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{V012}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{V013}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{V014}}{double COLUMN_DESCRIPTION}
+#'}
+#' @details DETAILS
+"DomicilioRenda"
