cb329de4de31b6b3a8b9a24431a9643bdda5b8dc
Felipe Figueiredo <philsf79@gmail.com>
2021-07-27 21:34:59 -0300

    broken: tentativa de mutate condicional (fail)

--- R/abrir_base_estado.R
+++ R/abrir_base_estado.R
@@ -4,8 +4,10 @@ abrir_base_estado <- function(base, estado, censo_dir = "~/Downloads/Censo2010/"
   b <- readxl::read_excel(arquivo, na = "X")
 
   b %>%
-    # fix Entorno03
-    mutate(Cod_UF = estado) %>%
+    # fix Basico: substitui Cod_UF pelo código (numérico)
+    # Assume que Cod_meso sempre existe na base em que Cod_UF existe e
+    # toma os 2 primeiros dígitos de um número de 4 dígitos
+    mutate( across(any_of(Cod_UF), Cod_meso %/% 100) ) %>%
     # fix Responsavel02_SP2
     mutate(across(starts_with("V"), as.numeric))
 }
