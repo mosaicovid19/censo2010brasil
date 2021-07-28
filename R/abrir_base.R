fd7f38542d065406fde8d3e5dee7b544a7ecd506
Felipe Figueiredo <philsf79@gmail.com>
2021-07-27 22:53:40 -0300

    lista de estados separada do código (clareza)

--- R/abrir_base.R
+++ R/abrir_base.R
@@ -2,11 +2,14 @@ abrir_base <- function(base, estados = NULL, censo_dir, cores = 1) {
 
   require(doParallel) # construto %dopar%
 
-  estados <- match.arg(estados, several.ok = TRUE)
+  estados_full <- c("AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA",
+                    "MG", "MS", "MT", "PA", "PB", "PE", "PI", "PR", "RJ", "RN",
+                    "RO","RR", "RS", "SC", "SE", "SP1", "SP2", "TO")
+
+  estados <- match.arg(estados, estados_full, several.ok = TRUE)
+
   if (is.null(estados)) {
-    estados <- c("AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA",
-                 "MG", "MS", "MT", "PA", "PB", "PE", "PI", "PR", "RJ", "RN", "RO",
-                 "RR", "RS", "SC", "SE", "SP1", "SP2", "TO")
+    estados <- estados_full
   }
 
   # cores <- detectCores() %/% 2
