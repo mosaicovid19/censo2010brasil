2729d571c25a13b3e9839e69f365b37afabbac90
Felipe Figueiredo <philsf79@gmail.com>
2021-08-12 18:00:06 -0300

    Pessoa04

---
+++ data-raw/Pessoa04.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+
+## localização dos dados brutos
+if(!exists("censo_dir")) censo_dir <- file.path(paste0(Sys.getenv("HOME"), "/Downloads/Censo2010/"))
+
+if(!exists("cores")) cores <- 1
+
+Pessoa04 <- abrir_base(base = "Pessoa04", cores = cores)
+
+# Salvar apenas a estrutura do data.frame
+Pessoa04 <- Pessoa04 %>% head(0)
+usethis::use_data(Pessoa04, overwrite = TRUE)
+
+# template de doc (Rd)
+sinew::makeOxygen(Pessoa04, add_fields = "source")
