342225f04bdc112a924601c9d4907cb2bd2b93f3
Felipe Figueiredo <philsf79@gmail.com>
2021-08-12 20:06:09 -0300

    caminho para DB generalizado (OS independente)
    Esta sintaxe deve funcionar em todos os OS, inclusive Windows.

--- data-raw/sqlite_create.R
+++ data-raw/sqlite_create.R
@@ -1,5 +1,6 @@
 library(censo2010brasil)
 library(DBI)
-con <- dbConnect(odbc::odbc(), driver = "SQLite3", database = path.expand('~/Downloads/Censo2010/bases_censo.sqlite'))
+con <- dbConnect(odbc::odbc(), driver = "SQLite3", database = file.path(Sys.getenv("HOME"), "Downloads/Censo2010", "censo2010brasil.sqlite"))
+
 dbDisconnect(con)
 detach("package:censo2010brasil", unload = TRUE)
