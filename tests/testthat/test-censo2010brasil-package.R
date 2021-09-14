3e2fefc3a3629f77bd5b799c1b852d5c293eafda
Felipe Figueiredo <philsf79@gmail.com>
2021-09-14 19:18:15 -0300

    novos testes de integridade do db (tabelas)

--- tests/testthat/test-censo2010brasil-package.R
+++ tests/testthat/test-censo2010brasil-package.R
@@ -8,16 +8,33 @@ test_that("db file integrity", {
   expect_equal(tools::md5sum(dbfile), "b80e73d8a7d066fdecc545b35d9f0dfc", ignore_attr = TRUE)
 })
 
-test_that("connection (writable)", {
-  censodb <- dbConnect(
-    odbc::odbc(),
-    driver = "SQLite3",
-    database = dbfile
-  )
+## Connect to db
+censodb <- dbConnect(
+  odbc::odbc(),
+  driver = "SQLite3",
+  database = dbfile
+)
 
+test_that("connection (writable)", {
   expect_true(dbIsValid(censodb))
   expect_false(dbIsReadOnly(censodb))
-
-  dbDisconnect(censodb)
 })
 
+test_that("Tables", {
+  expect_equal(
+    dbListTables(censodb) %>% length(),
+    26
+  )
+  expect_equal(
+    dbListTables(censodb) %>% sort(),
+    c("Basico", "Domicilio01", "Domicilio02", "DomicilioRenda", "Entorno01",
+      "Entorno02", "Entorno03", "Entorno04", "Entorno05", "Pessoa01",
+      "Pessoa02", "Pessoa03", "Pessoa04", "Pessoa05", "Pessoa06", "Pessoa07",
+      "Pessoa08", "Pessoa09", "Pessoa10", "Pessoa11", "Pessoa12", "Pessoa13",
+      "PessoaRenda", "Responsavel01", "Responsavel02", "ResponsavelRenda"
+    )
+  )
+})
+
+## Disconnect from db
+dbDisconnect(censodb)
