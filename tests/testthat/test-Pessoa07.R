5fc0bdfb282d69d30294b88e7a0c6316901b84bf
Felipe Figueiredo <philsf79@gmail.com>
2021-09-14 06:53:02 -0300

    Pessoa07

--- tests/testthat/test-Pessoa07.R
+++ tests/testthat/test-Pessoa07.R
@@ -1,3 +1,88 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+library(DBI)
+censodb <- dbConnect(
+  odbc::odbc(),
+  driver = "SQLite3",
+  database = file.path(Sys.getenv("HOME"), "Downloads/Censo2010", "censo2010brasil.sqlite")
+)
+
+Pessoa07 <- tbl(censodb, "Pessoa07")
+
+test_that("connection (writable)", {
+  expect_true(dbIsValid(censodb))
+  expect_false(dbIsReadOnly(censodb))
 })
+
+test_that("class", {
+  expect_s3_class(Pessoa07, "tbl_SQLite")
+  expect_s3_class(Pessoa07, "tbl_dbi")
+  expect_s3_class(Pessoa07, "tbl_sql")
+  expect_s3_class(Pessoa07, "tbl_lazy")
+  expect_s3_class(Pessoa07, "tbl")
+})
+
+test_that("dimensions", {
+  expect_identical(
+    dim(Pessoa07),
+    c(NA, 256L)
+  )
+})
+
+test_that("nrow", {
+  expect_identical(
+    Pessoa07 %>%
+      count() %>%
+      pull(),
+    310077L # menos setores
+  )
+})
+
+test_that("names", {
+  expect_equal(
+    Pessoa07 %>%
+      colnames() %>%
+      length(),
+    256
+  )
+  expect_equal(
+    Pessoa07 %>%
+      select(starts_with("Cod_")) %>%
+      colnames() %>% length(),
+    1
+  )
+  expect_equal(
+    Pessoa07 %>%
+      select(starts_with("Nome_")) %>%
+      colnames() %>% length(),
+    0
+  )
+  expect_equal(
+    Pessoa07 %>%
+      select(starts_with("V")) %>%
+      colnames() %>% length(),
+    204
+  )
+  expect_equal(
+    Pessoa07 %>%
+      select(-starts_with(c("Cod_", "Situacao_Setor", "Nome_", "V"))) %>%
+      colnames() %>% length(),
+    50 # junk
+  )
+})
+
+test_that("keys types", {
+  expect_type(
+    Pessoa07 %>%
+      select(Cod_setor) %>%
+      head() %>%
+      pull(),
+    "double")
+  expect_type(
+    Pessoa07 %>%
+      select(Situacao_setor) %>%
+      head() %>%
+      pull(),
+    "double")
+})
+
+dbDisconnect(censodb)
+
