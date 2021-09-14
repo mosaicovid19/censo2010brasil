44a748838a7fb7507e45db864b890158f1772918
Felipe Figueiredo <philsf79@gmail.com>
2021-09-14 08:42:00 -0300

    testar integridade do arquivo (MD5SUM desativado devido ao tempo)

--- tests/testthat/test-censo2010brasil-package.R
+++ tests/testthat/test-censo2010brasil-package.R
@@ -1,11 +1,21 @@
 library(DBI)
+
+dbfile <- file.path(Sys.getenv("HOME"), "Downloads/Censo2010", "censo2010brasil.sqlite")
+
+test_that("db file integrity", {
+  expect(file.size(dbfile), 2269097984)
+  # expect_equal(as.character(tools::md5sum(dbfile)), "a34c89945037285bd463b6242233d017")
+})
+
 censodb <- dbConnect(
   odbc::odbc(),
   driver = "SQLite3",
-  database = file.path(Sys.getenv("HOME"), "Downloads/Censo2010", "censo2010brasil.sqlite")
+  database = dbfile
 )
 
 test_that("connection (writable)", {
   expect_true(dbIsValid(censodb))
   expect_false(dbIsReadOnly(censodb))
 })
+
+dbDisconnect(censodb)
